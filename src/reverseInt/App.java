adc743e0a5d379a6541e22fd32130511d6638dd2
macbook <elmirbekalisherov24@gmail.com>
2022-12-24 02:05:40 +0600

    v

--- src/reverseInt/App.java
+++ src/reverseInt/App.java
@@ -10,21 +10,15 @@ public class App {
         StringBuilder finish = new StringBuilder();
         char[] chars = String.valueOf( x ).toCharArray();
         for (char aChar : chars) {
-            if(aChar != '0') {
-                stringBuilder.append( aChar );
-            }
+            if(aChar != '0') {stringBuilder.append( aChar );}
         }
         int num = Integer.parseInt( String.valueOf( stringBuilder ) );
         char[] chars1 = String.valueOf( num ).toCharArray();
         if(num>=0){
-            for (int i = chars1.length - 1; i >= 0; i--) {
-                finish.append( chars1[i] );
-            }
+            for (int i = chars1.length - 1; i >= 0; i--) {finish.append( chars1[i] );}
             return Integer.parseInt(String.valueOf(finish));
         }else {
-            for (int i = chars1.length - 1; i >= 0; i--) {
-                finish.append( chars1[i] );
-            }
+            for (int i = chars1.length - 1; i >= 0; i--) {finish.append( chars1[i] );}
             return Integer.parseInt( "-"+finish.subSequence( 0,finish.length()-1 ));
         }
     }
