1016bc4105875a6567f536fe1ee7293d1a581d95
macbook <elmirbekalisherov24@gmail.com>
2022-12-30 01:42:08 +0600

    v

---
+++ src/numberOfCommonFactors/App.java
@@ -0,0 +1,23 @@
+package numberOfCommonFactors;
+
+public class App {
+
+    public static void main(String[] args) {
+        App app = new App();
+        System.out.println( app.commonFactors(25,30) );
+    }
+    public int commonFactors(int a, int b) {
+        while(b != 0)
+        {
+            if(a > b)
+            {
+                a = a - b;
+            }
+            else
+            {
+                b = b - a;
+            }
+        }
+        return a;
+    }
+}
