8cbe4641d71c632d1454029b2bee90f59ed6fd76
macbook <elmirbekalisherov24@gmail.com>
2023-01-03 17:11:09 +0600

    v

---
+++ src/binarySearch/App.java
@@ -0,0 +1,20 @@
+package binarySearch;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        App app = new App();
+        System.out.println( app.search( new int[]{-1, 0, 3, 5, 9, 12}, 3 ) );
+    }
+
+    public int search(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return - 1;
+    }
+}
