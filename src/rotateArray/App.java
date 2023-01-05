20443444f99b457370e794247899439a313197de
macbook <elmirbekalisherov24@gmail.com>
2023-01-05 16:21:40 +0600

    v

---
+++ src/rotateArray/App.java
@@ -0,0 +1,16 @@
+package rotateArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        App app = new App();
+        app.rotate( new int[] {1,2,3,4,5,6,7},3 );
+    }
+    public void rotate(int[] nums, int k) {
+        List<Integer> ostatok = new ArrayList<>();
+        List<Integer> all = new ArrayList<>();
+
+    }
+}
