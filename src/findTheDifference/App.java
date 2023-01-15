b6cf29cbeb9915e1339e4160bfc9069887a6c9b5
macbook <elmirbekalisherov24@gmail.com>
2023-01-15 18:54:08 +0600

    v

--- src/findTheDifference/App.java
+++ src/findTheDifference/App.java
@@ -1,8 +1,6 @@
 package findTheDifference;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 public class App {
 
@@ -12,23 +10,15 @@ public class App {
 
     }
     public char findTheDifference(String s, String t) {
-        char[] chars = s.toCharArray();
-        char[] chars1 = t.toCharArray();
-        List<String> list = new ArrayList<>();
-        List<String> list1 = new ArrayList<>();
-        List<String> lis = new ArrayList<>();
-        for (char ch:chars) {list.add( String.valueOf( ch ) );}
-        for (char ch:chars1) {list1.add( String.valueOf( ch ) );}
-        Collections.sort( list );
-        Collections.sort( list1 );
-        for (int i = 0; i < t.length(); i++) {
-            if(list.get( i ).equals( list1.get( i ) )){
-
-            }else {
-                lis.add( list1.get( i ) );
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        for(int i =0; i<sArray.length-1;i++){
+            if(sArray[i]!=tArray[i]){
+                return tArray[i];
             }
         }
-        char[] f = lis.get( 0 ).toCharArray();
-        return f[0];
+        return tArray[tArray.length-1];
     }
 }
