383f2641347793332e6f871af5946bd1aea8a779
macbook <elmirbekalisherov24@gmail.com>
2023-01-15 19:55:38 +0600

    v

---
+++ src/numberOfValidWordsInSentence/App.java
@@ -0,0 +1,13 @@
+package numberOfValidWordsInSentence;
+
+public class App {
+
+    public static void main(String[] args) {
+        App app = new App();
+        System.out.println( app.countValidWords( "cat and  dog" ) );
+    }
+
+    public int countValidWords(String sentence) {
+        return 0;
+    }
+}
