b30c1d0fc12c4ffdbaa134371fa2d6b598418e59
macbook <elmirbekalisherov24@gmail.com>
2023-01-23 12:49:35 +0600

    v

--- src/validPerfectSquare/App.java
+++ src/validPerfectSquare/App.java
@@ -4,9 +4,12 @@ public class App {
 
     public static void main(String[] args) {
         App app = new App();
+        System.out.println( app.isPerfectSquare(16) );
+        System.out.println( app.isPerfectSquare(14) );
 
     }
     public boolean isPerfectSquare(int num) {
+        double m=Math.sqrt(num);
 return num > 0;
     }
 }
