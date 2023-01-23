e89f6ac73c26f8f8f4b5ce1616541ac477299e58
macbook <elmirbekalisherov24@gmail.com>
2023-01-23 12:47:31 +0600

    v

--- src/validPerfectSquare/App.java
+++ src/validPerfectSquare/App.java
@@ -3,6 +3,9 @@ package validPerfectSquare;
 public class App {
 
     public static void main(String[] args) {
+        App app = new App();
+    }
+    public boolean isPerfectSquare(int num) {
 
     }
 }
